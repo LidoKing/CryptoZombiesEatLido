06c711d3cd0c9fe029e1a0dce6f5af96bdee07c2
LidoKing <kinglido203@gmail.com>
2022-02-18 22:48:38 +0800

    updatead migrations contract, discarded use of constructor

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -3,15 +3,15 @@
 pragma solidity >=0.8.0 <0.9.0;
 
 contract Migrations {
-  address public owner;
+  address public owner = msg.sender;
   uint public last_completed_migration;
 
-  constructor() {
-    owner = msg.sender;
-  }
-
   modifier restricted() {
-    if (msg.sender == owner) _;
+    require(
+      msg.sender == owner,
+      "This function is restricted to the contract's owner"
+    );
+    _;
   }
 
   function setCompleted(uint completed) public restricted {
