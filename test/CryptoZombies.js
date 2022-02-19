e86a8ce2ca14968d26336a57d36826f47222b55a
LidoKing <kinglido203@gmail.com>
2022-02-19 21:18:34 +0800

    passed tests for leveling up and changing zombie name

--- test/CryptoZombies.js
+++ test/CryptoZombies.js
@@ -1,66 +1,117 @@
 const CryptoZombies = artifacts.require("CryptoZombies");
 const utils = require("./helpers/utils");
 const time = require("./helpers/time");
+const web3 = require('web3');
 var expect = require('chai').expect;
 const zombieNames = ["Zombie 1", "Zombie 2"];
 
+async function createZombie() {
+  const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+  return result.logs[0].args.zombieId.toNumber();
+}
+
 contract("CryptoZombies", (accounts) => {
     let [alice, bob] = accounts;
-    let contractInstance;
+    let instance;
 
     beforeEach(async () => {
-      contractInstance = await CryptoZombies.new();
+      instance = await CryptoZombies.new();
     });
 
-    it("should be able to create a new zombie", async () => {
-        const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-        expect(result.receipt.status).to.equal(true);
-        expect(result.logs[0].args.name).to.equal(zombieNames[0]);
+    context("ZOMBIE CREATION", async () => {
+      it("should be able to create a new zombie", async () => {
+          const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+          expect(result.receipt.status).to.equal(true);
+          expect(result.logs[0].args.name).to.equal(zombieNames[0]);
+      });
+
+      it("should not allow two zombies", async () => {
+          await instance.createRandomZombie(zombieNames[0], {from: alice});
+          await utils.shouldThrow(instance.createRandomZombie(zombieNames[1], {from: alice}));
+      });
     });
 
-    it("should not allow two zombies", async () => {
-      //contractInstance = await CryptoZombies.new();
-        await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-        await utils.shouldThrow(contractInstance.createRandomZombie(zombieNames[1], {from: alice}));
+    context("ATTACK AND FEEDING", async () => {
+      it("zombies should be able to attack another zombie", async () => {
+          let result;
+          result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+          const firstZombieId = result.logs[0].args.zombieId.toNumber();
+          result = await instance.createRandomZombie(zombieNames[1], {from: bob});
+          const secondZombieId = result.logs[0].args.zombieId.toNumber();
+          await time.increase(time.duration.days(1));
+          await instance.attack(firstZombieId, secondZombieId, {from: alice});
+          expect(result.receipt.status).to.equal(true);
+      });
     });
 
-    it("zombies should be able to attack another zombie", async () => {
-        let result;
-        result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-        const firstZombieId = result.logs[0].args.zombieId.toNumber();
-        result = await contractInstance.createRandomZombie(zombieNames[1], {from: bob});
-        const secondZombieId = result.logs[0].args.zombieId.toNumber();
-        await time.increase(time.duration.days(1));
-        await contractInstance.attack(firstZombieId, secondZombieId, {from: alice});
-        expect(result.receipt.status).to.equal(true);
-    });
+    context("ZOMBIE TRANSFER", async () => {
+      context("with the single-step transfer scenario", async () => {
+          it("should transfer a zombie", async () => {
+              // create a zombie
+              const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+              const zombieId = result.logs[0].args.zombieId.toNumber();
+              // owner calls transfer
+              await instance.transferFrom(alice, bob, zombieId, {from: alice});
+              // get owner of zombie
+              const newOwner = await instance.ownerOf(zombieId);
+              expect(newOwner).to.equal(bob);
+          });
+      });
 
-    context("with the single-step transfer scenario", async () => {
-        it("should transfer a zombie", async () => {
-            const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
+      context("with the two-step transfer scenario", async () => {
+        it("should approve and then transfer a zombie when the approved address calls transferForm", async () => {
+            // create a zombie
+            const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
             const zombieId = result.logs[0].args.zombieId.toNumber();
-            await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
-            const newOwner = await contractInstance.ownerOf(zombieId);
+            // approve recepient
+            await instance.approve(bob, zombieId, {from: alice});
+            // recepient calls transfer
+            await instance.transferFrom(alice, bob, zombieId, {from: bob});
+            // get owner of zombie
+            const newOwner = await instance.ownerOf(zombieId);
             expect(newOwner).to.equal(bob);
         });
+        
+        it("should approve and then transfer a zombie when the owner calls transferForm", async () => {
+            // create a zombie
+            const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+            const zombieId = result.logs[0].args.zombieId.toNumber();
+            // approve recepient
+            await instance.approve(bob, zombieId, {from: alice});
+            // owner calls transfer
+            await instance.transferFrom(alice, bob, zombieId, {from: alice});
+            // get owner of zombie
+            const newOwner = await instance.ownerOf(zombieId);
+            expect(newOwner).to.equal(bob);
+        });
+      });
     });
 
-    context("with the two-step transfer scenario", async () => {
-      it("should approve and then transfer a zombie when the approved address calls transferForm", async () => {
-          const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-          const zombieId = result.logs[0].args.zombieId.toNumber();
-          await contractInstance.approve(bob, zombieId, {from: alice});
-          await contractInstance.transferFrom(alice, bob, zombieId, {from: bob});
-          const newOwner = await contractInstance.ownerOf(zombieId);
-          expect(newOwner).to.equal(bob);
+    context("ZOMBIE UTILS", async () => {
+      it("should level up zombie after payment", async () => {
+        // create a zombie
+        const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+        const zombieId = result.logs[0].args.zombieId.toNumber();
+        // level up zombie
+        await instance.levelUp(zombieId, {from: alice, value: web3.utils.toWei("0.001", "ether")});
+        // get zombie level
+        const zombie = await instance.zombies(zombieId);
+        const level = zombie.level.toNumber();
+        expect(level).to.equal(2);
       });
-      it("should approve and then transfer a zombie when the owner calls transferForm", async () => {
-          const result = await contractInstance.createRandomZombie(zombieNames[0], {from: alice});
-          const zombieId = result.logs[0].args.zombieId.toNumber();
-          await contractInstance.approve(bob, zombieId, {from: alice});
-          await contractInstance.transferFrom(alice, bob, zombieId, {from: alice});
-          const newOwner = await contractInstance.ownerOf(zombieId);
-          expect(newOwner).to.equal(bob);
+
+      it("should change name of zombie", async () => {
+        // create a zombie
+        const result = await instance.createRandomZombie(zombieNames[0], {from: alice});
+        const zombieId = result.logs[0].args.zombieId.toNumber();
+        // level up zombie to level 2
+        await instance.levelUp(zombieId, {from: alice, value: web3.utils.toWei("0.001", "ether")});
+        // change zombie name
+        await instance.changeName(zombieId, "new name", {from: alice});
+        // get new zombie name
+        const zombie = await instance.zombies(zombieId);
+        const newName = zombie.name;
+        expect(newName).to.equal("new name");
       });
     });
 });
