c896e32387e2673f38bfe178704d7e7e9a433878
LidoKing <kinglido203@gmail.com>
2022-02-22 20:35:43 +0800

    added function for allowing zombie to attack immediately

--- hardhat/contracts/main/cryptozombies.sol
+++ hardhat/contracts/main/cryptozombies.sol
@@ -5,6 +5,11 @@ pragma solidity >=0.8.0 <0.9.0;
 import "./zombieownership.sol";
 
 contract CryptoZombies is ZombieOwnership {
+  function readyAttack(uint _id) external onlyOwner {
+    Zombie storage theZombie = zombies[_id];
+    theZombie.readyTime = block.timestamp;
+  }
+
   function kill() external onlyOwner {
     address payable owner = payable(owner());
     selfdestruct(owner);
